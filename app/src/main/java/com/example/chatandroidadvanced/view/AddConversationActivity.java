93e75975d85bcb2e4ff8f967603395c636c84cf9
Michael Stiftinger <michael.stiftinger@gmx.at>
2019-02-23 18:47:42 +0100

    added internet permission to manifest added glide to gradle

---
+++ app/src/main/java/com/example/chatandroidadvanced/view/AddConversationActivity.java
@@ -0,0 +1,70 @@
+package com.example.chatandroidadvanced.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.chatandroidadvanced.model.Conversation;
+import com.example.chatandroidadvanced.viewmodel.ContactListAdapter;
+import com.example.chatandroidadvanced.R;
+
+import java.util.LinkedList;
+
+public class AddConversationActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerview;
+    private ContactListAdapter contactListAdapter;
+    private final LinkedList<Conversation> contactList = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_conversation);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbarNewConversation);
+        setSupportActionBar(toolbar);
+
+        recyclerview = findViewById(R.id.rcvContacts);
+        contactListAdapter = new ContactListAdapter(this, contactList);
+        recyclerview.setAdapter(contactListAdapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        //Todo get list of contacts from service with get method
+        //contactList = get....;
+
+        //todo delete this example list
+        //only for testing self defined fix list.
+        int wordListSize = contactList.size();
+        contactList.addLast(new Conversation("12345","Roman", "Leight", "romanleight@gmx.at", "", ""));
+        contactList.addLast(new Conversation("45678","Ben", "Friedman", "benfriedman@gmx.at", "", ""));
+        recyclerview.getAdapter().notifyItemInserted(wordListSize);
+        recyclerview.smoothScrollToPosition(wordListSize);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_conversation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.btnCancelAddConversation){
+            //cancel activity of creating new conversation and go back to list of conversations
+            Toast.makeText(getApplicationContext() ,"Cancel create conversation!", Toast.LENGTH_LONG).show();
+            Intent intentConversations = new Intent(this, ConversationActivity.class);
+            startActivity(intentConversations);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/chatandroidadvanced/view/ConversationActivity.java
@@ -0,0 +1,71 @@
+package com.example.chatandroidadvanced.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.chatandroidadvanced.R;
+import com.example.chatandroidadvanced.model.Conversation;
+import com.example.chatandroidadvanced.viewmodel.ContactListAdapter;
+import com.example.chatandroidadvanced.viewmodel.ConversationListAdapter;
+
+import java.util.LinkedList;
+
+public class ConversationActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerview;
+    private ConversationListAdapter conversationListAdapter;
+    private final LinkedList<Conversation> conversationList = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbarConversations);
+        setSupportActionBar(toolbar);
+
+        recyclerview = findViewById(R.id.rcvConversations);
+        conversationListAdapter = new ConversationListAdapter(this, conversationList);
+        recyclerview.setAdapter(conversationListAdapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        //Todo get list of conversations from server with get method should be done everytime this activity is started to get actual list
+        //conversationslist = get....;
+
+        //todo delete this example list
+        //only for testing self defined fix list.
+        int wordListSize = conversationList.size();
+        conversationList.addLast(new Conversation("12345","Jim", "Rogers", "jimrogers@gmx.com", "Bla bla bla..", "22:42"));
+        conversationList.addLast(new Conversation("45678","Lula", "Lein", "lulalein@gmx.com","Keine ahnung..", "23:19"));
+        recyclerview.getAdapter().notifyItemInserted(wordListSize);
+        recyclerview.smoothScrollToPosition(wordListSize);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_conversation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.btnAddConversation){
+            Toast.makeText(getApplicationContext() ,"Add new conversation!", Toast.LENGTH_LONG).show();
+            //create new conversation if button is clicked
+            Intent intentNewConversation = new Intent(this, AddConversationActivity.class);
+            startActivity(intentNewConversation);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
